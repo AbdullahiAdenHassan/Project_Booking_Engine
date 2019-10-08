09cd8206b894598714a985b7df200357decf7676
Abdullahi Aden Hassan <Abdullahi.Aden.Hassan@gmail.com>
2019-10-08 22:42:40 +0200

    Clean version on master

--- src/java/ScoreCode.java
+++ src/java/ScoreCode.java
@@ -6,24 +6,31 @@ public class ScoreCode {
     public static void main(String[] args) {
         Rooms rooms = new Rooms();
         Booking newBooking = new Booking();
-
-        do {
-            switch (newBooking.menu()) {
+        String Input = newBooking.menu();
+        while(!Input.equals("4"))
+        {
+            switch (Input) {
                 case "1":
                     //list all rooms
                     rooms.listOfAllRooms();
+                    Input = newBooking.menu();
                     break;
                 case "2":
                     //Book a room
 
+                    Input = newBooking.menu();
                     break;
                 case "3":
                     //List all booked rooms
+
+                    Input = newBooking.menu();
                     break;
                 default:
                     //wrong number
+
+                    Input = newBooking.menu();
                     break;
             }
-        } while (!newBooking.menu().equals("4"));
+        }
     }
 }
