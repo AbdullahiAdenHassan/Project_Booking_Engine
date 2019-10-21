6dc7835e6128319224d2eb451447e49c2bc02238
Abdullahi Aden Hassan <Abdullahi.Aden.Hassan@gmail.com>
2019-10-21 20:05:58 +0200

    Trying to use LocalDate parse

--- src/java/Hotel/Rooms.java
+++ src/java/Hotel/Rooms.java
@@ -1,22 +1,23 @@
 package Hotel;
 
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Rooms {
     private static HashMap<String, String> m_reservation = new HashMap();
-    private Guest m_guest;
     private List<String> m_roomslist = Arrays.asList("101", "102", "103", "104", "105");
-    private ListIterator<String> m_rooms = m_roomslist.listIterator();
-    private Iterator m_hotelRooms;
-    private Iterator m_bookedRooms;
+    private List<String> m_bookedRooms = new ArrayList<>();
+    private List<LocalDate> m_dates = new ArrayList<>();
+    private Guest m_guest;
     private String m_room;
-    private int m_id;
+    private String m_date;
 
     public Rooms() {
-        while (m_rooms.hasNext()) {
-            m_room = m_rooms.next();
-            m_reservation.put(m_room, "");
-        }
+        for (String rooms : m_roomslist)
+            m_reservation.put(rooms, "");
     }
 
     public Rooms(Guest guest) {
@@ -25,44 +26,51 @@ public class Rooms {
     }
 
     public void listOfAllRooms() {
-        m_hotelRooms = m_reservation.entrySet().iterator();
-        System.out.println("Rooms:");
-        while (m_hotelRooms.hasNext()) {
-            Map.Entry roomsNumber = (Map.Entry) m_hotelRooms.next();
-            System.out.println("\t" + roomsNumber.getKey().toString());
-        }
+        System.out.println("List of all rooms: ");
+        m_roomslist.forEach((k) -> System.out.println("\t" + k));
     }
 
     public void getARoomIfPossible() {
-        if (isThatRoomFree(getRoomNumber()) == true)
+        if (isThatRoomFreeTest(getRoomNumber(), m_date) == true)
             System.out.println("You have successfully booked room nr " + getRoomNumber() + "!");
         else
             System.out.println("That room is not free!");
     }
 
     public void listOfAllBookedRooms() {
-        System.out.println("ID " + " Room" + "  Name");
-        m_bookedRooms = m_reservation.entrySet().iterator();
-        m_id = 1;
-        while (m_bookedRooms.hasNext()) {
-            Map.Entry paris = (Map.Entry) m_bookedRooms.next();
-            if (paris.getValue().toString().matches("[a-zA-Z0-9]+")) {
-                System.out.println(m_id++ + "   " + paris.getKey() + "   " + paris.getValue());
-            }
+        System.out.println("\nList of all booked rooms: ");
+        m_bookedRooms = m_reservation
+                .entrySet()
+                .stream()
+                .filter(m_reservation -> !m_reservation.getValue().isEmpty())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        m_bookedRooms.forEach(x -> System.out.println("Room " + x));
+    }
+
+    private boolean isDateFormatterCorrect(String date){
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+        try {
+            //LocalDate.parse(date, format);
+            m_dates.add(LocalDate.parse(date, format));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
-    private boolean isThatRoomFree(String roomNumber) {
-        if (isRoomFree(roomNumber)) {
+    private boolean isThatRoomFreeTest(String roomNumber, String date) {
+
+        if (isDateFormatterCorrect(date)) {
+            m_reservation.replace(roomNumber, "", m_guest.getName());
+            m_dates.add(LocalDate.parse(date));
+            System.out.println(m_dates.add(LocalDate.parse(date)));
+            return true;
+        } else if (m_dates.contains(date) & !m_bookedRooms.contains(roomNumber)) {
             m_reservation.replace(roomNumber, "", m_guest.getName());
             return true;
         }
-        return false;
-    }
-
-    private boolean isRoomFree(String roomNumber) {
-        if (m_reservation.containsKey(roomNumber) & m_reservation.get(roomNumber).isEmpty())
-            return true;
         return false;
     }
 
@@ -72,5 +80,9 @@ public class Rooms {
 
     private String getRoomNumber() {
         return m_room;
+    }
+
+    public void setDate(String date) {
+        this.m_date = date;
     }
 }
